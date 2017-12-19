641d0fcf18f41dcd16961ce5e60b579dd52b2823
Ray Bellis <ray@isc.org>
2017-12-19 11:33:20 +0000

    added some jsdocs

--- index.js
+++ index.js
@@ -4,6 +4,15 @@
 
 const RNDC_Protocol = require('./lib/protocol');
 
+/**
+ * Makes a connection to the given host and port and initiates the
+ * rndc protocol with the given key and algorithm.
+ * @param {string} host - the BIND9 server hostname
+ * @param {number} port - the BIND9 server's rndc port
+ * @param {string} key - the rndc shared key, in base64 format
+ * @param {string} algo - the rndc shared algorithm (e.g. "hmac-md5")
+ * @returns {RNDC_Protocol} - an RDNC_Protocol session object
+ */
 function connect(host, port, key, algo) {
 	return new RNDC_Protocol(host, port, key, algo);
 }
